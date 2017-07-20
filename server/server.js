acb394e9d884c09020d8c629a60d3c23a196b9d4
Naveen Dayananda <ad.naveen@gmail.com>
2017-07-20 19:06:19 +0530

    Added socket.io to server and client

--- server/server.js
+++ server/server.js
@@ -1,17 +1,34 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 const port = process.env.PORT || 3000;
 
+/**
+ * While using socket.io then we have to use http to create server.
+ * http is built-in module in Node.js designed to support many features of the protocol which have been traditionally difficult to use. 
+ * Creating server was handled in app.listen method inside express, while using express. But to use socket we have to do it explicitly.
+ */
+const server = http.createServer(app);
+
+// After creating the server, we need to pass it to socketIO
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.send('index.html');
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('Disconnected from client');
+    });
 });
 
-app.listen(port, () => {
+// While using socket.io and express we have to call listen method on server insted of app
+server.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
 });
 
