1e0f2b8c50911ad02922c8ea29dcb0a0a7ef7cf5
Naveen Dayananda <ad.naveen@gmail.com>
2017-07-24 17:47:12 +0530

    Create message and broadcast message

--- public/js/index.js
+++ public/js/index.js
@@ -1,19 +1,14 @@
-(function(global, undefined) {
+//(function(global, undefined) {
     var socket = io();
     socket.on('connect', function() {
         console.log('Connected to server');
 
         socket.on('newMessage', function(msg) {
             console.log('New Message: ', msg);
-        })
-
-        socket.emit('createMessage', {
-            to: 'Malini',
-            text: 'Hey! How are u?'
         });
     });
 
     socket.on('disconnect', function() {
         console.log('Disconnected from server');
     })
-})(window);
+//})(window);
--- server/server.js
+++ server/server.js
@@ -22,14 +22,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Naveen',
-        text: 'Hey! Whats up?',
-        createdAt: 123456
-    });
-
     socket.on('createMessage', function(msg) {
         console.log('Create Message: ', msg);
+        io.emit('newMessage', {
+            from: msg.from,
+            text: msg.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
